7a15e5e8da7b617c168f2060bd844addf9defb0d
Saif Al Jadiri <39760012+saifaljadiri@users.noreply.github.com>
2018-08-22 01:32:42 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Glashandel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_restant = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gewoon Glas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Speciaal Glas";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Totaal Bedrag ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(241, 103);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 49);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(241, 182);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 49);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(241, 273);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(162, 49);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(241, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 67);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Bereken";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 68);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(471, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Restante glas";
+            // 
+            // lbl_restant
+            // 
+            this.lbl_restant.AutoSize = true;
+            this.lbl_restant.Location = new System.Drawing.Point(504, 171);
+            this.lbl_restant.Name = "lbl_restant";
+            this.lbl_restant.Size = new System.Drawing.Size(26, 29);
+            this.lbl_restant.TabIndex = 9;
+            this.lbl_restant.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 597);
+            this.Controls.Add(this.lbl_restant);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_restant;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Glashandel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        int gewoonGlas = 30;
+        int snijKostenGewoon = 10;
+        int speciaalGlas = 55;
+        int snijKostenSpeciaal = 25;
+        double korting = 0.95;
+        double restantGlas = 0;
+        double kostenGewoonGlas = 0;
+        double kostenSpeciaalGlas = 0;
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Empty(textBox1))
+            {
+                double aantalM2 = Convert.ToDouble(textBox1.Text);
+                double totaalKosten = 0;
+                if (IsRestantGlasOver(aantalM2))
+                {
+                    totaalKosten = BerekenRestantGlas(aantalM2, gewoonGlas);
+                }
+                else
+                {
+                    totaalKosten = Math.Ceiling(aantalM2) * gewoonGlas + snijKostenGewoon;
+                    if (aantalM2 - Math.Floor(aantalM2) == 0)
+                    {
+                        restantGlas += 0;
+                    }
+                    else
+                    {
+                        restantGlas += 1 - (aantalM2 - Math.Floor(aantalM2));
+                    }
+                    lbl_restant.Text = Convert.ToString(restantGlas);
+                }
+
+
+                // if statment als bedrag 145 hoger is hoef je geen snijkostengewoon tebtalen//
+                if (totaalKosten >= 145)
+                {
+                    totaalKosten -= snijKostenGewoon;
+                }
+                if (totaalKosten >= 250) //als bedrag hoger dan 250 dan komt 5% korting op het totale bedrag//
+                {
+                    totaalKosten *= korting;
+                }
+                kostenGewoonGlas = totaalKosten;
+                textBox3.Text = Convert.ToString(kostenGewoonGlas + kostenSpeciaalGlas);
+            }
+            else
+            {
+                kostenGewoonGlas = 0;
+                textBox3.Text = Convert.ToString(kostenGewoonGlas + kostenSpeciaalGlas);
+            }
+
+            if (!Empty(textBox2))
+            {
+                double aantalM2 = Convert.ToDouble(textBox2.Text);
+                double totaalKosten = 0;
+                if (IsRestantGlasOver(aantalM2))
+                {
+                    totaalKosten = BerekenRestantGlas(aantalM2, speciaalGlas);
+                }
+                else
+                {
+                    totaalKosten = Math.Ceiling(aantalM2) * speciaalGlas + snijKostenSpeciaal;
+                    if (aantalM2 - Math.Floor(aantalM2) == 0)
+                    {
+                        restantGlas += 0;
+                    }
+                    else
+                    {
+                        restantGlas += 1 - (aantalM2 - Math.Floor(aantalM2));
+                    }
+                    lbl_restant.Text = Convert.ToString(restantGlas);
+                }
+
+
+                // if statment als bedrag 145 hoger is hoef je geen snijkostengewoon tebtalen//
+                if (totaalKosten >= 145)
+                {
+                    totaalKosten -= snijKostenSpeciaal;
+                }
+                if (totaalKosten >= 250) //als bedrag hoger dan 250 dan komt 5% korting op het totale bedrag//
+                {
+                    totaalKosten *= korting;
+                }
+                kostenSpeciaalGlas = totaalKosten;
+                textBox3.Text = Convert.ToString(kostenGewoonGlas + kostenSpeciaalGlas);
+            }
+            else
+            {
+                kostenSpeciaalGlas = 0;
+                textBox3.Text = Convert.ToString(kostenGewoonGlas + kostenSpeciaalGlas);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            kostenGewoonGlas = 0;
+            kostenSpeciaalGlas = 0;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private bool Empty(TextBox textbox)
+        {
+            if (string.IsNullOrWhiteSpace(textbox.Text))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool IsRestantGlasOver(double aantalM2)
+        {
+            if (restantGlas >= aantalM2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private double BerekenRestantGlas(double aantalM2, double glasKosten)
+        {
+            double kostenRestantGlas = 0;
+            double restKosten = 0;
+
+            // bereken de kosten voor het restante glas.
+            kostenRestantGlas = glasKosten * restantGlas;
+
+            // bereken de kosten voor het overgehouden (niet restante) glas.
+            aantalM2 -= restantGlas;
+            restantGlas = 0;
+            restKosten = Math.Ceiling(aantalM2) * glasKosten;
+
+            // bepaal de nieuwe restante glas
+            if (aantalM2 - Math.Floor(aantalM2) == 0)
+            {
+                restantGlas = 0;
+            }
+            else
+            {
+                restantGlas = 1 - (aantalM2 - Math.Floor(aantalM2));
+            }
+
+            // update de restantGlas label
+            lbl_restant.Text = Convert.ToString(restantGlas);
+
+            // beiden kosten returnen
+            return kostenRestantGlas + restKosten;
+        }
+    }
+}
